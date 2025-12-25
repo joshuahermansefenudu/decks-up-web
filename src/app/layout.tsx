76089b6ceebb89cb772d47b65ec4890fd85622e5
Joshua <joshuahefenudu@gmail.com>
2025-12-24 21:12:43 -0500

    Add AdSense verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-8332039790897527" />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8332039790897527"
